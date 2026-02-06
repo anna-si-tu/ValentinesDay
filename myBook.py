8bba4cdd223b7a929c1d5fc702280291c26ff3d8
Anna Si Tu <asitu03@student.ubc.ca>
2026-02-06 02:17:17 -0800

    Added page numbers

--- myBook.py
+++ myBook.py
@@ -17,20 +17,22 @@ def next_page(event = None):    # Create a function for the next page
     global current_page         # Use variable existing outside of fnc
     if current_page < len(pages) - 1:
         current_page += 1
-        label.config(text = pages[current_page])
+        text.config(text = pages[current_page])
+        pgNum.config(text=f"Page {current_page + 1} / {len(pages)}")
 # Render next page when fnc is called only if another page exists
 
 def prev_page(event = None):
     global current_page
     if current_page > 0:
         current_page -= 1
-        label.config(text=pages[current_page])
+        text.config(text = pages[current_page])
+        pgNum.config(text=f"Page {current_page + 1} / {len(pages)}")
 
 book = tk.Tk()                  # Create a tinker object
 book.title("Why I Love You")    # Create title on window popup
 book.geometry("1000x600")       # Determine window size
 
-label = tk.Label(
+text = tk.Label(
     book,
     text = pages[current_page],
     font = ("Times New Roman", 16),
@@ -38,11 +40,20 @@ label = tk.Label(
                                     # before going to the next line
     justify = "left"                # left-aligned
 )
-label.pack(expand = True)
+text.pack(expand = True)
+
+pgNum = tk.Label(
+    book,
+    text = f"Page {current_page + 1} / {len(pages)}",
+    font=("Times New Roman", 12),
+    justify="right"
+)
+pgNum.pack(anchor="se", padx=20, pady=10)
 
 # Bind fnc to key buttons
 book.bind("<Button-1>", next_page)
 book.bind("<Right>", next_page)
 book.bind("<Left>", prev_page)
+book.focus_set()
 
 book.mainloop()     # Runs application until closed
